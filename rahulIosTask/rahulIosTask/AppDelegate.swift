6156f719e7b731721bbda151d8493a41d047a94c
Rahul <brpatel0832@gmail.com>
2020-07-07 11:17:05 +0530

    initial commit

---
+++ rahulIosTask/rahulIosTask/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  rahulIosTask
+//
+//  Created by Rahul Patel on 07/07/20.
+//  Copyright © 2020 Rahul Patel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+  //MARK: - App Lifecycle
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+
+}
+
